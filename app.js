510af13d952c4b7e08d7a65c5a378d30e630f490
edriso <edriso.dev@gmail.com>
2023-03-09 15:55:25 +0200

    add morgan package

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const toursRouter = require('./routes/toursRoutes');
 
 const app = express();
 
+// Middleware
+if (process.env.NODE_ENV === 'development') {
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
+// Routes
 app.use('/api/v1/tours', toursRouter);
 
 const port = process.env.PORT || 3000;
