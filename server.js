966a01519d5f7b193a1394a2f11dc06047ad1b30
edriso <edriso.dev@gmail.com>
2023-03-11 22:01:50 +0200

    refactor connecting to db

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log('Connected to DB...');
+    await mongoose
+      .connect(process.env.MONGO_URL)
+      .then(() => console.log('Connected to DB...'));
     app.listen(port, () => console.log(`Server is running on port ${port}...`));
   } catch (error) {
     console.log(error);
