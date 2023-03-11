58eaaaebf18576d295c342e62075125e5281f31b
edriso <edriso.dev@gmail.com>
2023-03-11 21:23:49 +0200

    add eslint-diable-next-line no-console

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ require('dotenv').config();
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
