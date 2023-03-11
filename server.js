34d0107868397179d695ad92eceabbbc9ef98cd2
edriso <edriso.dev@gmail.com>
2023-03-11 21:44:12 +0200

    connect to db

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
+// eslint-disable-next-line no-console
+mongoose.connect(process.env.MONGO_URL).then(console.log('Connected to DB...'));
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
