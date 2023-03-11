69cd56e479454821084b7bb18a6b6fc162218c64
edriso <edriso.dev@gmail.com>
2023-03-11 21:56:26 +0200

    add async startServer()

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-// eslint-disable-next-line no-console
-mongoose.connect(process.env.MONGO_URL).then(console.log('Connected to DB...'));
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Server is running on port ${port}...`));
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('Connected to DB...');
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
