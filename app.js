7d306bc940e5894eccbf98edc260570b9c8fa3de
edriso <edriso.dev@gmail.com>
2023-03-11 11:51:10 +0200

    add server.js as a good practice; things related to express in app.js, things related to server in server.js

--- app.js
+++ app.js
@@ -15,5 +15,4 @@ app.use(express.json());
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server is running on port ${port}...`));
+module.exports = app;
