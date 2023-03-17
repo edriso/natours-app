e31028b2eb8e721d071eb4cc2568014d3a7ac6ec
edriso <edriso.dev@gmail.com>
2023-03-18 00:24:48 +0200

    change error sent on development and production

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,11 +1,37 @@
+const sendError = (err, res) => {
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  } else if (err.isOperational) {
+    // Operational, trusted error: send message to client
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or unknown error: don't leak error details and send generic message
+    console.error('ERROR:', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  sendError(err, res);
+  // res.status(err.statusCode).json({
+  //   status: err.status,
+  //   message: err.message,
+  // });
 };
 
 module.exports = errorHandler;
