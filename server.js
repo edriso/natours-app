f078374e42402e1911567aefa610f43214bdd852
edriso <edriso.dev@gmail.com>
2023-03-18 15:09:58 +0200

    handle unhandled rejections, and uncaught exception errors

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT REJECTION! 💥');
+  console.log(err);
+  // process.exit(1); // becuz server now is in 'unclean state', in production there should be a tool to restart the server when crashed
+});
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-const start = async () => {
+const startServer = async () => {
   try {
     await mongoose
       .connect(process.env.MONGO_URL)
@@ -12,8 +19,13 @@ const start = async () => {
 
     app.listen(port, () => console.log(`Server is running on port ${port}...`));
   } catch (error) {
-    console.log(error);
+    console.log(error.name, error.message);
   }
 };
 
-start();
+startServer();
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥');
+  console.log(err.name, err.message);
+});
